e6f39cf0b69a2e4c73f378f12befa3ef9420c120
DevikaChhipa <chhipadevika0@gmail.com>
2025-10-29 15:55:58 +0530

    all services page complete

--- src/app/allservices/page.js
+++ src/app/allservices/page.js
@@ -8,26 +8,31 @@ export default function AllServices() {
 
   return (
     <>
-      <section
-        className="relative bg-contain bg-black bg-no-repeat h-[90vh] "
-        style={{
-          backgroundImage:
-            "url('/Image/allservices/allservices-bg-banner.png')",
-        }}
-      >
-        <div className="relative z-10 flex flex-col md:flex-row items-center justify-between gap-10">
-          <div className=" text-[#C5C5C5] py-20 lg:py-30  px-6 md:px-12 lg:px-20">
-            <h1 className="text-4xl md:text-5xl  leading-tight mb-4">
-              Pampering Starts <br /> with One Tap
-            </h1>
-            <p className="text-sm md:text-base my-10">
-              Lorem ipsum quia dolor sit porro quisquam est qui amet consectetur
-              adipisci, sed quia duis aute irure dolor in reprehenderit dolore
-              magna aliqua.
-            </p>
-          </div>
-        </div>
-      </section>
+   <section
+      className="relative bg-no-repeat bg-cover bg-center min-h-[90vh] flex items-center px-6 md:px-20"
+      style={{
+        backgroundImage: "url('/Image/allservices/allservices-bg-banner.png')",
+      }}
+    >
+
+
+
+      <div className="relative w-1/3 z-10 text-[#C5C5C5] max-w-xl">
+        <h1 className="text-3xl md:text-5xl lg:pt-20 font-bold mb-4 leading-tight">
+         Pampering
+Starts with
+One Tap
+        </h1>
+        <p className="text-sm  mb-6 text-[#C5C5C5]">
+   Lorem ipsum quia dolor sit porro quisquam est qui amet consectetur adipisci, sed quia duis aute irure dolor in reprehenderit dolore magna aliqua
+        </p>
+
+<div className="absolute -bottom-10 left-136 bg-black w-50 h-50 border-[40px] border-[#F6EFE4] rounded-full ">
+ Because you Deserve the Best Touch
+</div>
+
+      </div>
+    </section>
 
 <Services />
     </>
