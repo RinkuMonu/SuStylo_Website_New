4919c646e103a0132f60e0b989ec14ca1d88f8d9
DevikaChhipa <chhipadevika0@gmail.com>
2025-12-22 17:02:16 +0530

    salon list

--- components/freelancerservices.js
+++ components/freelancerservices.js
@@ -810,7 +810,7 @@ useEffect(() => {
         }
     };
     return (
-        <section className="bg-[#f6efe4] py-10">
+        <section id="servicesection" className="bg-[#f6efe4] py-10">
             <h2 className="text-3xl font-bold mb-8 text-center text-[#5F3F31]">Services</h2>
 
             <div className="grid grid-cols-1 md:grid-cols-3 gap-8 max-w-6xl mx-auto">
